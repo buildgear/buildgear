d57b0afb5b0fbd949a183ae84adde8da39db9582
Martin Lund <martin.lund@keep-it-simple.com>
2012-06-24 00:48:13 +0200

    Updated buildgear.sh to GPLv3

--- src/buildgear.sh
+++ src/buildgear.sh
@@ -4,13 +4,10 @@
 #
 #  Copyright (c) 2011-2012 Martin Lund <martin.lund@keep-it-simple.com>
 #  Copyright (c) 2000-2005 Per Liden
-# 
-#  This script file is originally written for pkgutils by Per Liden.
-#  It is adopted and rewritten for Build Gear.
 #
-#  This program is free software; you can redistribute it and/or modify
+#  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
+#  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
 #  This program is distributed in the hope that it will be useful,
@@ -19,9 +16,7 @@
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, 
-#  USA.
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 bg_put() {
