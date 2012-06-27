110dc15b1ae061bcbfba6c457e695bc4b80ee615
Martin Lund <martin.lund@keep-it-simple.com>
2012-06-27 10:36:55 +0200

    Cleaned up tabs in thread class.

--- src/include/buildgear/thread.h
+++ src/include/buildgear/thread.h
@@ -15,35 +15,36 @@ using namespace std;
 
 class Runnable {
 public:
-	virtual void* run() = 0;
-	virtual ~Runnable() = 0;
+   virtual void* run() = 0;
+   virtual ~Runnable() = 0;
 };
+
 
 class Thread {
 public:
-	Thread(auto_ptr<Runnable> run, bool isDetached = false);
-	Thread(bool isDetached = false);
-	virtual ~Thread();
-	void start();
-	void* join();
+   Thread(auto_ptr<Runnable> run, bool isDetached = false);
+   Thread(bool isDetached = false);
+   virtual ~Thread();
+   void start();
+   void* join();
 private:
-	// thread ID
-	pthread_t PthreadThreadID;
-	// runnable object will be deleted automatically
-	auto_ptr<Runnable> runnable;
-	// true if thread created in detached state
-	bool detached;
-	pthread_attr_t threadAttribute;
-	Thread(const Thread&);
-	const Thread& operator=(const Thread&);
-	// called when run() completes
-	void setCompleted();
-	// stores return value from run()
-	void* result;
-	virtual void* run() { return NULL; }
-	static void* startThreadRunnable(void* pVoid);
-	static void* startThread(void* pVoid);
-	void printError(string msg, int status, string fileName, int lineNumber);
+   // thread ID
+   pthread_t PthreadThreadID;
+   // true if thread created in detached state
+   bool detached;
+   pthread_attr_t threadAttribute;
+   // runnable object will be deleted automatically
+   auto_ptr<Runnable> runnable;
+   Thread(const Thread&);
+   const Thread& operator=(const Thread&);
+   // called when run() completes
+   void setCompleted();
+   // stores return value from run()
+   void* result;
+   virtual void* run() {}
+   static void* startThreadRunnable(void* pVoid);
+   static void* startThread(void* pVoid);
+   void printError(const char * msg, int status, const char* fileName, int lineNumber);
 };
 
 #endif
