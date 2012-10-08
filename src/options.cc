ba4bb046b66cbb36c42d4ec5c265ba335d13b1d3
Martin Lund <martin.lund@keep-it-simple.com>
2012-10-08 12:33:41 +0200

    Updated license

--- src/options.cc
+++ src/options.cc
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2011-2012  Martin Lund
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 #include "config.h"
 #include <iostream>
 #include <stdlib.h>
@@ -290,7 +308,7 @@ void COptions::ShowVersion(void)
    cout << "Build Gear " << VERSION << "\n";
    cout << "Copyright (C) 2011-2012 Martin Lund\n";
    cout << "\n";
-   cout << "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n";
+   cout << "License GPLv2+: GNU GPL version 2 or later <http://gnu.org/licenses/gpl-2.0.html>.\n";
    cout << "This is free software: you are free to change and redistribute it.\n";
    cout << "There is NO WARRANTY, to the extent permitted by law.\n";
    cout << "\n";
