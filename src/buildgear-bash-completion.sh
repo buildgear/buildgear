ef098b67cd3271d7ee72809523ce480b15958277
Martin Lund <martin.lund@keep-it-simple.com>
2012-11-07 21:03:30 +0100

    Added bash autocompletion support

---
+++ src/buildgear-bash-completion.sh
@@ -0,0 +1,47 @@
+#
+#  Bash auto completion script for buildgear:
+#
+#  buildgear [command] [options]
+#
+
+_buildgear()
+{
+   local cur prev
+
+   COMPREPLY=()
+   cur=${COMP_WORDS[COMP_CWORD]}
+   prev=${COMP_WORDS[COMP_CWORD-1]}
+
+   commands="download build clean show init"
+   options="--help --version"
+   download_options="--all"
+   build_options="--keep-work --update-checksum --update-footprint --no-strip --no-fakeroot --all"
+   clean_options="--all"
+   show_options="--build-order --download-order --dependency --readme --version --log --log-tail"
+
+   if [ $COMP_CWORD -eq 1 ]; then
+     COMPREPLY=( $(compgen -W "$commands $options" -- $cur) )
+   elif [ $COMP_CWORD -eq 2 ]; then
+     case "$prev" in
+       "download")
+         COMPREPLY=( $(compgen -W "$download_options" -- $cur) )
+         ;;
+       "build")
+         COMPREPLY=( $(compgen -W "$build_options" -- $cur) )
+         ;;
+       "clean")
+         COMPREPLY=( $(compgen -W "$clean_options" -- $cur) )
+         ;;
+       "show")
+         COMPREPLY=( $(compgen -W "$show_options" -- $cur) )
+         ;;
+       "init")
+         ;;
+       *)
+         ;;
+     esac
+   fi
+
+   return 0
+} &&
+complete -F _buildgear buildgear
