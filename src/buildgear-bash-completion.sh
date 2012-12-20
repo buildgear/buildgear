5f4162f76701b2dbd1987a4136335632e3b6a62e
Martin Lund <martin.lund@keep-it-simple.com>
2012-12-20 19:25:45 +0100

    Added help to autocompletion.

--- src/buildgear-bash-completion.sh
+++ src/buildgear-bash-completion.sh
@@ -1,7 +1,7 @@
 #
 #  Bash auto completion script for buildgear:
 #
-#  buildgear [command] [options]
+#  buildgear <command> [<options>] [<build name>]
 #
 
 _buildgear()
@@ -13,13 +13,15 @@ _buildgear()
    prev=${COMP_WORDS[COMP_CWORD-1]}
    command=${COMP_WORDS[1]}
 
-   commands="download build clean show init"
+   commands="download build clean show init help"
    options="--help --version"
    download_options="--all"
-   build_options="--keep-work --update-checksum --update-footprint --no-strip --no-fakeroot --all"
+   build_options="--keep-work --update-checksum --update-footprint --no-strip
+                  --no-fakeroot --all"
    clean_options="--all"
    show_options="--build-order --download-order --dependency --readme --version
                  --log --log-tail --log-mismatch"
+   help_options=$commands
 
    if [ $COMP_CWORD -eq 1 ]; then
      COMPREPLY=( $(compgen -W "$commands $options" -- $cur) )
@@ -38,6 +40,9 @@ _buildgear()
          COMPREPLY=( $(compgen -W "$show_options" -- $cur) )
          ;;
        "init")
+         ;;
+       "help")
+         COMPREPLY=( $(compgen -W "$help_options" -- $cur) )
          ;;
        *)
          ;;
