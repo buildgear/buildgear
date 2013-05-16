44fb4d9696240b6c8d666d4bc69ee31ac3f41db8
Jesper Larsen <knorr.jesper@gmail.com>
2013-05-16 19:25:53 +0200

    Updated Copyright information

--- src/cursor.cc
+++ src/cursor.cc
@@ -1,7 +1,7 @@
 /*
  * This file is part of Build Gear.
  *
- * Copyright (c) 2011-2013  Martin Lund
+ * Copyright (c) 2012-2013  Jesper Larsen
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
